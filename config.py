cd90e665282ba359f0a72ef726a4ada5282947b0
Jueuunn7 <wndmsrla1234@gmail.com>
2025-02-10 04:43:39 +0900

    add: kafka producer

--- config.py
+++ config.py
@@ -14,5 +14,8 @@ DB_PASS = os.environ.get('DB_PASS')
 
 EUREKA_HOST = os.environ.get('EUREKA_HOST')
 EUREKA_PORT = os.environ.get('EUREKA_PORT')
-EUREKA_SERVICE_NAME=os.environ.get('EUREKA_SERVICE_NAME')
-INSTANCE_PORT=int(os.environ.get('INSTANCE_PORT'))
+EUREKA_SERVICE_NAME = os.environ.get('EUREKA_SERVICE_NAME')
+INSTANCE_PORT = int(os.environ.get('INSTANCE_PORT'))
+
+KAFKA_HOST = os.environ.get('KAFKA_HOST')
+KAFKA_PORT = os.environ.get('KAFKA_PORT')
