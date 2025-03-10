8a4c280944604fcad2bff433cb1bdb6fec0b7e08
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-11 02:56:09 +0900

    refector: event consume service

--- event/consumer.py
+++ event/consumer.py
@@ -1,7 +1,6 @@
 import json
 
 from aiokafka import AIOKafkaConsumer
-from kafka import KafkaConsumer
 
 from config import KAFKA_HOST, KAFKA_PORT
 from db import get_session
@@ -9,8 +8,6 @@ from event.schema.fast import CreateStageFast
 from event.schema.official import CreateStageOfficial
 from src.minigame.presentation.schema.minigame import MinigameCreateReq
 from src.minigame.service.minigame import MinigameService
-
-
 
 
 async def consume():
@@ -24,19 +21,20 @@ async def consume():
     try:
         async for msg in consumer:
             if msg.topic == 'create_stage_fast':
-                await EventController.create_stage_fast(msg)
+                await EventConsumeService.create_stage(
+                    CreateStageFast(**json.loads(msg.value.decode('utf-8')))
+                )
             elif msg.topic == 'create_stage_official':
-                await EventController.create_stage_official(msg)
+                await EventConsumeService.create_stage(
+                    CreateStageOfficial(**json.loads(msg.value.decode('utf-8')))
+                )
     finally:
         await consumer.stop()
 
 
-class EventController:
+class EventConsumeService:
     @staticmethod
-    async def create_stage_fast(msg):
-        data = CreateStageFast(
-            **json.loads(msg.value.decode('utf-8'))
-        )
+    async def create_stage(data):
         session = await get_session()
         await MinigameService(session).create_minigame(
             MinigameCreateReq(
@@ -44,20 +42,5 @@ class EventController:
                 is_active_plinko=data.miniGame.isPlinkoActive,
                 is_active_yavarwee=data.miniGame.isYavarweeActive,
                 is_active_coin_toss=data.miniGame.isCoinTossActive
-            )
-        )
-
-    @staticmethod
-    async def create_stage_official(msg):
-        data = CreateStageOfficial(
-            **json.loads(msg.value.decode('utf-8'))
-        )
-        session = await get_session()
-        await MinigameService(session).create_minigame(
-            MinigameCreateReq(
-                stage_id=data.stageId,
-                is_active_plinko=data.is_active_plinko,
-                is_active_yavarwee=data.is_active_yavarwee,
-                is_active_coin_toss=data.is_active_coin_toss
             )
         )
