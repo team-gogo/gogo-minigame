66d8a587b5b7d80738b3338cb37a6f5f49f7e21a
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-11 12:25:50 +0900

    feat: stage minigame model

--- src/minigame/domain/model/minigame.py
+++ src/minigame/domain/model/minigame.py
@@ -1,11 +1,19 @@
+from enum import Enum
+
 from sqlmodel import Field, SQLModel
+
+
+class MinigameStatus(Enum):
+    ACTIVE = "ACTIVE"
+    PENDING = "PENDING"
 
 
 class Minigame(SQLModel, table=True):
     minigame_id: int = Field(default=None, primary_key=True)
     stage_id: int = Field(unique=True)
-    is_active_coin_toss: bool
-    is_active_plinko: bool
-    is_active_yavarwee: bool
+    is_active_coin_toss: bool = Field(default=False)
+    is_active_plinko: bool = Field(default=False)
+    is_active_yavarwee: bool = Field(default=False)
+    status: MinigameStatus = Field(default=MinigameStatus.PENDING)
 
     __tablename__ = 'tbl_minigame'
