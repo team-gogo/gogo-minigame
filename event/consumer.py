f04d8f6bdf6c3aa82c3c154d351872ccb88ca335
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-12 17:34:37 +0900

    feat: ticket

--- event/consumer.py
+++ event/consumer.py
@@ -3,17 +3,17 @@ import json
 from aiokafka import AIOKafkaConsumer
 
 from config import KAFKA_HOST, KAFKA_PORT
-from db import get_session
+from event.controller import EventConsumeController
 from event.schema.fast import CreateStageFast
 from event.schema.official import CreateStageOfficial
 from event.schema.stage import StageConfirmReq
-from src.minigame.presentation.schema.minigame import MinigameCreateReq
-from src.minigame.service.minigame import MinigameService
+from event.schema.ticket import TicketShopBuyReq
 
 
 async def consume():
     consumer = AIOKafkaConsumer(
         'stage_create_fast', 'stage_create_official', 'stage_confirm',
+        'ticket_shop_buy', 'ticket_buy_point_minus',
         bootstrap_servers=f'{KAFKA_HOST}:{KAFKA_PORT}'
     )
 
@@ -28,34 +28,10 @@ async def consume():
                 await EventConsumeController.create_stage_official(CreateStageOfficial(**data))
             elif msg.topic == 'stage_confirm':
                 await EventConsumeController.stage_confirm(StageConfirmReq(**data))
+            elif msg.topic == 'ticket_shop_buy':
+                await EventConsumeController.ticket_buy(TicketShopBuyReq(**data))
+
     finally:
         await consumer.stop()
 
 
-class EventConsumeController:
-    @staticmethod
-    async def create_stage_fast(data: CreateStageFast):
-        session = await get_session()
-        await MinigameService(session).create_minigame(
-            MinigameCreateReq(
-                stage_id=data.stageId,
-                is_active_coin_toss=data.miniGame.isCoinTossActive
-            )
-        )
-
-    @staticmethod
-    async def create_stage_official(data):
-        session = await get_session()
-        await MinigameService(session).create_minigame(
-            MinigameCreateReq(
-                stage_id=data.stageId,
-                is_active_plinko= data.miniGame.isPlinkoActive,
-                is_active_yavarwee=data.miniGame.isYavarweeActive,
-                is_active_coin_toss=data.miniGame.isCoinTossActive
-            )
-        )
-
-    @staticmethod
-    async def stage_confirm(data: StageConfirmReq):
-        session = await get_session()
-        await MinigameService(session).confirm_minigame(data.stageId)
--- src/ticket/domain/model/ticket.py
+++ src/ticket/domain/model/ticket.py
@@ -1,14 +1,15 @@
 from typing import Optional
 
+from sqlalchemy import Column, BigInteger
 from sqlmodel import SQLModel, Field
 
 
 class Ticket(SQLModel, table=True):
     ticket_id: Optional[int] = Field(default=None, primary_key=True)
     minigame_id: Optional[int] = Field(foreign_key='tbl_minigame.minigame_id', ondelete='CASCADE')
-    user_id: int
-    coin_toss_ticket_amount: int
-    plinko_ticket_amount: int
-    yavarwee_ticket_amount: int
+    user_id: int = Field(sa_column=Column(BigInteger()))
+    coin_toss_ticket_amount: int = Field(sa_column=Column(BigInteger(), default=0))
+    plinko_ticket_amount: int = Field(sa_column=Column(BigInteger(), default=0))
+    yavarwee_ticket_amount: int = Field(sa_column=Column(BigInteger(), default=0))
 
     __tablename__ = 'tbl_minigame_ticket'
