14c6939aafba111fa0bd00aeb202f27ad3ef10e4
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-15 09:53:12 +0900

    update: event producer.py

--- event/producer.py
+++ event/producer.py
@@ -1,6 +1,6 @@
-from aiokafka import AIOKafkaProducer
 import json
 
+from aiokafka import AIOKafkaProducer
 from pydantic import BaseModel
 
 from config import KAFKA_HOST, KAFKA_PORT
@@ -8,8 +8,12 @@ from config import KAFKA_HOST, KAFKA_PORT
 
 class EventProducer:
     @staticmethod
-    async def create_event(topic, msg: BaseModel):
+    async def create_event(topic: str, key: str, value: BaseModel):
         producer = AIOKafkaProducer(bootstrap_servers=f'{KAFKA_HOST}:{KAFKA_PORT}')
         await producer.start()
-        await producer.send_and_wait(topic, json.dumps(msg.dict()).encode('utf-8'))
+        await producer.send_and_wait(
+            key=key,
+            topic=topic,
+            value=json.dumps(value.dict()).encode('utf-8'),
+        )
         await producer.stop()
