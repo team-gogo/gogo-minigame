5ebda1ac8b95b9967e4de951b8599e6247c8d6a8
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-15 11:21:59 +0900

    update: kafka consumer.py

--- event/consumer.py
+++ event/consumer.py
@@ -14,28 +14,24 @@ from event.schema.ticket import TicketShopBuyReq
 async def consume():
     consumer = AIOKafkaConsumer(
         'stage_create_fast', 'stage_create_official', 'stage_confirm',
-        'ticket_shop_buy', 'ticket_buy_point_minus',
+        'ticket_point_minus',
         bootstrap_servers=f'{KAFKA_HOST}:{KAFKA_PORT}'
     )
 
     await consumer.start()
-
     try:
         async for msg in consumer:
-            try:
-                data = json.loads(msg.value.decode('utf-8'))
-                if msg.topic == 'stage_create_fast':
-                    await EventConsumeController.create_stage_fast(CreateStageFast(**data))
-                elif msg.topic == 'stage_create_official':
-                    await EventConsumeController.create_stage_official(CreateStageOfficial(**data))
-                elif msg.topic == 'stage_confirm':
-                    await EventConsumeController.stage_confirm(StageConfirmReq(**data))
-                elif msg.topic == 'ticket_shop_buy':
-                    await EventConsumeController.ticket_buy(TicketShopBuyReq(**data))
-            except Exception as e:
-                logging.error(f'kafka consume format error = ' + str(e))
-
+            data = json.loads(msg.value.decode('utf-8'))
+            logging.info(f'Consume kafka data {data.topic} value: {data}')
+            if msg.topic == 'stage_create_fast':
+                await EventConsumeController.create_stage_fast(CreateStageFast(**data))
+            elif msg.topic == 'stage_create_official':
+                await EventConsumeController.create_stage_official(CreateStageOfficial(**data))
+            elif msg.topic == 'stage_confirm':
+                await EventConsumeController.stage_confirm(StageConfirmReq(**data))
+            elif msg.topic == 'ticket_point_minus':
+                await EventConsumeController.ticket_buy(TicketShopBuyReq(**data))
+    except Exception as e:
+        logging.exception(f'Kafka consume exception {str(e)}')
     finally:
         await consumer.stop()
-
-
