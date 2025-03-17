8bb79c7886ea0405ddafa4ed78d7356e00ffec46
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-17 13:53:59 +0900

    refactor: EventProducer class

--- event/producer.py
+++ event/producer.py
@@ -8,14 +8,22 @@ from config import KAFKA_HOST, KAFKA_PORT
 
 
 class EventProducer:
+    producer: AIOKafkaProducer
+
+    @staticmethod
+    async def get_producer():
+        if not EventProducer.producer:
+            EventProducer.producer = AIOKafkaProducer(bootstrap_servers=f'{KAFKA_HOST}:{KAFKA_PORT}')
+            await EventProducer.producer.start()
+            logging.info(f'Producer is started.')
+        return EventProducer.producer
+
     @staticmethod
     async def create_event(topic: str, key: str, value: BaseModel):
-        producer = AIOKafkaProducer(bootstrap_servers=f'{KAFKA_HOST}:{KAFKA_PORT}')
-        await producer.start()
+        producer = await EventProducer.get_producer()
         await producer.send_and_wait(
             key=key.encode('utf-8'),
             topic=topic,
             value=json.dumps(value.dict()).encode('utf-8'),
         )
         logging.info(f'Kafka producer Send {topic} value: {value}')
-        await producer.stop()
