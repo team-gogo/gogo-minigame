8b679dc9c9771bcb1fd35a3301be965e53a3740d
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-28 10:21:53 +0900

    update: db driver

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from config import DB_HOST, DB_NAME, DB_USER, DB_PASS
 
 
-DB = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
+DB = f'mysql+aiomysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}'
 
 async_engine = create_async_engine(DB, future=True, echo=True, poolclass=NullPool)
 
