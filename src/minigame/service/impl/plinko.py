63f39c26b2cec8a9e6029b873e8f601d0bb25565
Jueuunn7 <wndmsrla1234@gmail.com>
2025-03-29 23:49:14 +0900

    fix: plinko bet response amount

--- src/minigame/service/impl/plinko.py
+++ src/minigame/service/impl/plinko.py
@@ -115,7 +115,7 @@ class PlinkoMinigameBetServiceImpl(MinigameBetService):
             )
 
             return PlinkoBetRes(
-                amount=result,
+                amount=changed_point + earned_point - losted_point,
                 path=['L' if p==-1 else 'R' for p in path],
                 multi=index_
             )
