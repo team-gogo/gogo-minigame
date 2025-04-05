72f03e2693b46268152f014b0b65ded6bbd23d09
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-06 03:01:32 +0900

    feat: bet limit controller

--- src/minigame/presentation/controller/minigame.py
+++ src/minigame/presentation/controller/minigame.py
@@ -18,3 +18,13 @@ async def minigame_active_game(
         session: Annotated[AsyncSession, Depends(get_session)]
 ):
     return await MinigameService(session).get_active_minigame(stage_id, request_user_id)
+
+
+@router.get('/minigame/bet-limit/{stage_id}')
+async def minigame_active_game(
+        stage_id: int,
+        request_user_id: Annotated[int, Header()],
+        authority: Annotated[str, Depends(authority_student)],
+        session: Annotated[AsyncSession, Depends(get_session)]
+):
+    return await MinigameService(session).get_bet_limit(stage_id, request_user_id)
