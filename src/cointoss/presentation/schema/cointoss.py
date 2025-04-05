96042c65eaace28f518122b42251451920c82cb4
Jueuunn7 <wndmsrla1234@gmail.com>
2025-04-06 01:42:31 +0900

    feat: coin toss bet req schema

--- src/cointoss/presentation/schema/cointoss.py
+++ src/cointoss/presentation/schema/cointoss.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel
 
 
+class CoinTossBetType(BaseModel):
+    FRONT = 'FRONT'
+    BACK = 'BACK'
+
+
 class CoinTossBetReq(BaseModel):
     amount: int
+    bet: CoinTossBetType
 
 
 class CoinTossBetRes(BaseModel):
